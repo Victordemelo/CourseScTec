6b5ea75d42782378b5930b0538933b82c5e9fb30
Victor de Melo da Rosa <147112756+Victordemelo@users.noreply.github.com>
2026-02-17 09:31:19 -0300

    Feat: Realizando exercicio de array.

---
+++ front-End/exercicios/exercicio_js/ex02/script.js
@@ -0,0 +1,14 @@
+const frutas = ["Uva", "banana", "maça", "Goiaba", "morango"]
+console.log(frutas)
+console.log(frutas[2])
+frutas[1] = "Abacaxi"
+console.log(frutas)
+frutas.push("Laranja", "Melancia")
+console.log(frutas)
+
+// frutas.pop() para remover o ultimo item do array
+// frutas.forEach(fruta => console.log(fruta)); // para imprimir cada item do array usando o forEach
+// frutas.length para saber o tamanho do array
+// frutas.indexOf("Goiaba") para saber o indice do item "Goiaba" no array
+// frutas.sort() para ordenar o array em ordem alfabetica
+// frutas.reverse() para inverter a ordem do array
