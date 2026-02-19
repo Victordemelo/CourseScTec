8a846ab4ae686b48a5c72ea6a3a81b2a74ddac6d
Victor de Melo <victor.rosa.system@gmail.com>
2026-02-18 21:49:00 -0300

    Feat: Realizando exercicio com o app.js e commons

--- node/moduloExemplos/app.js
+++ node/moduloExemplos/app.js
@@ -1,12 +1,18 @@
 let pessoa = require("./commons/pessoa");
 let soma = require("./commons/soma");
-let imposto = require("./commons/calculoImposto")
+let imposto = require("./commons/calculoImposto");
+let calculadora = require("./commons/calculadora");
 
-victor = pessoa();
-soma = soma(2,5);
+// victor = pessoa();
+// soma = soma(2,5);
 
-console.log(JSON.stringify(victor));
-console.log(JSON.stringify(soma)); 
-console.log("Valor do produto com imposto: " + imposto.adicionar(10));
-console.log("Valor do imposto: " + imposto.valor(10));
-console.log("Taxa do imposto: " + imposto.imposto)
+// console.log(JSON.stringify(victor));
+// console.log(JSON.stringify(soma)); 
+// console.log("Valor do produto com imposto: " + imposto.adicionar(10));
+// console.log("Valor do imposto: " + imposto.valor(10));
+// console.log("Taxa do imposto: " + imposto.imposto)
+
+console.log("Realizando a soma de valores: " + calculadora.soma(2, 2));
+console.log("Realizando subtracao: " + calculadora.subtracao(10, 2));
+console.log("Realizando subtracao: " + calculadora.multiplicacao(3, 5));
+console.log("Realizando subtracao: " + calculadora.divisao(10, 2));
